d5f4ab9ab13f65600e127b56cdc08729836f02c1
Juan Molina <jmolina@initiumsoft.com>
2026-06-28 10:40:35 -0500

    test: add F5 unit tests for CuentaMovimientoService including F3 validation

---
+++ ms-cuentas/src/test/java/com/bp/mscuentas/MsCuentasApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bp.mscuentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+        "spring.jpa.hibernate.ddl-auto=none",
+        "spring.flyway.enabled=false"
+})
+class MsCuentasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
